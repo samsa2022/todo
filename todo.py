5eb711ded88d09307faad30b121ab7871ee01ccc
samsa2022 <samusenkokirill54@gmail.com>
2026-02-23 10:41:06 +0300

    Выход и меню

---
+++ todo.py
@@ -0,0 +1,10 @@
+tasks = []
+while True:
+    print('---- Список дел ----')
+    print('1. Показать задачи')
+    print('2. Добавить задачу')
+    print('3. Выход')
+    choice = input('Выберите действие: ')
+    if choice == 3:
+        print('До свидания!')
+        break
